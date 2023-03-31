f8ff206de8f5bf0a42a95e3ee2078f5be07885e0
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-03-30 20:19:40 -0700

    yo

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// create a pool of available characters. 
-function generatePassword(){
-  let ltrs = confirm("would you like caps? type yes if yes, type no if no.");
+ function generatePassword(){
+  let capltrs = confirm("would you like caps?");
+console.log(capltrs)
+let ltrs = confirm("would you like lowercase letters?")
 console.log(ltrs)
-let num = confirm("would you like numbers? type yes if yes, type no if no.")
+let num = confirm("would you like numbers?")
 console.log(num)
-let spc = confirm("would you like special charachters? type yes if yes, type no if no.")
+let spc = confirm("would you like special charachters?")
 console.log(spc)
 //let passwrdl = prompt("how long would you like your password to be? choose between 8 and 128")
 //console.log(passwrdl)
@@ -13,14 +14,29 @@ console.log(spc)
   var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var numbers = ["1","2","3","4","5","6","7","8","9"];
   var spchar = ["!","@","#","$","%","&","*","?",",",".","-","_","=","+","/","<",">"];
-  var userchoices = ["yes", "no"]
-  // figure out what characters we can use
-  //var pos = capletters + letters + numbers + userchoices;
-  var index = Math.floor(Math.random() * capletters.length);
-  var computerChoice = capletters[index];
-  writePassword();
+  
+  var pos = []
 
-return;
+  if (ltrs = true){
+   pos = pos.concat(letters);
+  }
+  if (num = true){
+    pos = pos.concat(numbers)
+  }
+  if (capltrs = true){
+    pos = pos.concat(capletters)
+  }
+  if (spc = true){
+    pos = pos.concat(spchar)
+  }
+  
+    let result = "";
+    for ( let i = 0; i < pos.length; i++ ) {
+        result += pos[Math.floor(Math.random() * pos.length)];
+    }
+    console.log("result" + result);
+    return result
+  
 }
 var generateBtn = document.querySelector("#generate");
 
